a7c50a612ea45e59776beb60614ea5021f8f9558
Antoine LAFOUASSE <antoine.lafouasse@icloud.com>
2015-02-10 19:04:25 -0800

    Implementing weight switch on tune.sh

--- tune.sh
+++ tune.sh
@@ -4,9 +4,23 @@ min=${3:-1}
 incr=${4:-1}
 max=${5:-200}
 
-shift $((OPTIND-1))
+weight_var=${6:-empty}
+
+weight_neighbors=""
+if [ $weight_var = "-w" ]
+then
+    weight_neighbors="--weight-neighbors"
+fi
+
+#while getopts ':w' opt ; do
+#  case $opt in
+#    w) weight_neighbors="--weight-neighbors" ;;
+#  esac
+#done
+## skip over the processed options
+#shift $((OPTIND-1))
 
 for k in $(seq $min $incr $max)
 do
-    ./classdoc_knn_canevas.py --k $k $1 $2
+    ./classdoc_knn_canevas.py --k $k $1 $2 $weight_neighbors
 done
