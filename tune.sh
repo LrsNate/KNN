22060de7340348f41f87db79ee13652cfc7ec8d3
Antoine LAFOUASSE <antoine.lafouasse@icloud.com>
2015-02-10 19:11:39 -0800

    Implementing tune.sh help

--- tune.sh
+++ tune.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ $# -eq 0 ] || [ $1 = "-h" ]
+then
+    echo "Usage:" $0 "train_set test_set MIN_K INCR_K MAX_K [-w]"
+    exit
+fi
+
 min=${3:-1}
 incr=${4:-1}
 max=${5:-200}
@@ -12,15 +18,11 @@ then
     weight_neighbors="--weight-neighbors"
 fi
 
-#while getopts ':w' opt ; do
-#  case $opt in
-#    w) weight_neighbors="--weight-neighbors" ;;
-#  esac
-#done
-## skip over the processed options
-#shift $((OPTIND-1))
-
 for k in $(seq $min $incr $max)
 do
     ./classdoc_knn_canevas.py --k $k $1 $2 $weight_neighbors
+    if [ $? -ne 0 ]
+    then
+        exit
+    fi
 done
